fbed9e24cd2afa0483740591abb236e3caffc880
noura <nourahalsuwailih@AiralkhasbNorah.net>
2021-02-01 19:34:37 +0300

    your message here

---
+++ story.js
@@ -0,0 +1,15 @@
+let name1 = "Mshary";
+let age = 25;
+let hobby = "Meditation";
+
+let x = age + 30; // add 30
+
+console.log(
+  "Mr./Mrs. " +
+    name1 +
+    ", in 33 years, you will be" +
+    x +
+    "years old. By then you'll have decades of " +
+    hobby +
+    "practice!"
+);
