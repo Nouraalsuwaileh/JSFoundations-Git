31ab040ce6d46499cc672270f497b1a3f97d5bec
noura <nourahalsuwailih@MacBook-Air-alkhas-b-Norah.local>
2021-02-02 18:40:42 +0300

    Solved the conditinals task

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let x = 10;
+let y = 5;
+let str = "+";
+
+if (str === "+") {
+  console.log("its +");
+} else if (str === "/") {
+  console.log(x / y);
+} else {
+  // "*"
+  console.log(x * y);
+}
